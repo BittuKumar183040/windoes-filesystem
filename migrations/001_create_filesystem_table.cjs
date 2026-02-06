0c59934a23f688aa4cdc5f323f83d3ab4060ea0a
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2026-02-07 05:24:30 +0530

    Added migration and include drive folders

--- migrations/001_create_filesystem_table.cjs
+++ migrations/001_create_filesystem_table.cjs
@@ -19,33 +19,56 @@ exports.up = function (knex) {
       table.unique(['parentId', 'name']);
     })
     .then(() => {
-      return knex('filesystem').insert([
-        {
-          id: 'c_drive',
-          userId: 'global',
-          parentId: null,
-          name: 'C:',
-          type: 'FOLDER',
-          size: 244322607104,
-          status: 'ACTIVE',
-          icon: "drive-harddisk",
-          createdAt: Math.floor(Date.now() / 1000),
-          updatedAt: Math.floor(Date.now() / 1000)
-        },
-        {
-          id: 'd_drive',
-          userId: 'global',
-          parentId: null,
-          name: 'D:',
-          type: 'FOLDER',
-          size: 14322607104,
-          status: 'ACTIVE',
-          icon: "drive-harddisk",
-          createdAt: Math.floor(Date.now() / 1000),
-          updatedAt: Math.floor(Date.now() / 1000)
-        }
-      ]);
-    });
+  const now = Math.floor(Date.now() / 1000);
+
+  return knex('filesystem').insert([
+    {
+      id: 'c_drive', userId: 'global', parentId: null, name: 'C:',
+      type: 'FOLDER', size: 244322607104, status: 'ACTIVE', icon: 'drive-harddisk',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'd_drive', userId: 'global', parentId: null, name: 'D:',
+      type: 'FOLDER', size: 24322607104, status: 'ACTIVE', icon: 'drive-harddisk',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'programfiles', userId: 'global', parentId: 'c_drive', name: 'Program Files',
+      type: 'FOLDER', size: 452224343, status: 'ACTIVE', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'programfilesx', userId: 'global', parentId: 'c_drive', name: 'Program Files (x86)',
+      type: 'FOLDER', size: 343524321, status: 'ACTIVE', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'windows', userId: 'global', parentId: 'c_drive', name: 'Windows',
+      type: 'FOLDER', size: 18345220352, status: 'ACTIVE', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'users', userId: 'global', parentId: 'c_drive', name: 'Users',
+      type: 'FOLDER', size: 122334455, status: 'DISABLED', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'programdata', userId: 'global', parentId: 'c_drive', name: 'ProgramData',
+      type: 'FOLDER', size: 22334455, status: 'ACTIVE', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'perflogs', userId: 'global', parentId: 'c_drive', name: 'PerfLogs',
+      type: 'FOLDER', size: 1048576, status: 'ACTIVE', icon: 'folder',
+      createdAt: now, updatedAt: now
+    },
+    {
+      id: 'onedrivetemp', userId: 'global', parentId: 'c_drive', name: 'OneDriveTemp',
+      type: 'FOLDER', size: 1324576, status: 'HIDDEN', icon: 'folder',
+      createdAt: now, updatedAt: now
+    }
+    ]);
+  });
 };
 
 exports.down = function (knex) {
